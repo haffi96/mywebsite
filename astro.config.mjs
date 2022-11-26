25dffe0ced55d0244e681f6a07535fc7ffb88bd6
haffi96 <haffimazhar96@gmail.com>
2022-11-26 10:31:52 +0000

    Add-resume-and-projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,17 @@
 import { defineConfig } from 'astro/config';
-import astroRemark from "@astrojs/markdown-remark";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        extendDefaultPlugins: true,
-        rehypePlugins: [
-            "rehype-slug",
-            [
-                "rehype-autolink-headings",
-                { behavior: "append"},
-            ],
-            [
-                "rehype-toc",
-                { headings: ["h1", "h2", "h3"] }
-            ]
-        ],
-    },
+  markdown: {
+    extendDefaultPlugins: true,
+    // rehypePlugins: ["rehype-slug", ["rehype-autolink-headings", {
+    //   behavior: "append"
+    // }], ["rehype-toc", {
+    //   headings: ["h1", "h2", "h3"]
+    // }]],
+  },
+  integrations: [mdx()]
 });
