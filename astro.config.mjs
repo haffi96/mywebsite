805c7e63e4088c3fa513906daddd58535ff76e70
Haffi Mazhar <haffimazhar96@gmail.com>
2023-02-15 20:31:26 +0000

    Add tailwind, upgrade to astroV2 and update styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,18 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   markdown: {
-    extendDefaultPlugins: true,
+    extendDefaultPlugins: true
     // rehypePlugins: ["rehype-slug", ["rehype-autolink-headings", {
     //   behavior: "append"
     // }], ["rehype-toc", {
     //   headings: ["h1", "h2", "h3"]
     // }]],
   },
-  integrations: [mdx()]
+
+  integrations: [mdx(), tailwind()]
 });
