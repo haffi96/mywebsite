88d6e516686803fc967ceff8cc5aa48c2f9efe3e
Haffi Mazhar <haffimazhar96@gmail.com>
2023-06-17 10:57:57 +0100

    blog metrics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // Plugins
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
-
 // https://astro.build/config
 export default defineConfig({
   markdown: {},
   server: { port: 8000 },
-  integrations: [
-    mdx({ gfm: true }),
-    tailwind(),
-    preact({ compat: true })
-  ]
+  integrations: [mdx({ gfm: true }), tailwind(), preact({ compat: true })],
 });
